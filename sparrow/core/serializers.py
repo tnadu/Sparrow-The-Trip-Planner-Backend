2c497a4cc12c527a26d731d116a5a052538d6f04
Toma <92331145+playback0022@users.noreply.github.com>
2023-04-05 19:36:11 +0300

    WriteMemberSerializer and WriteGroupSerializer written; awaiting testing

--- sparrow/core/serializers.py
+++ sparrow/core/serializers.py
@@ -26,8 +26,24 @@ class SmallMemberSerializer(serializers.ModelSerializer):
         fields = ['baseUser', 'profilePhoto', 'birthDate']
 
 
+# used for post/put/patch on the Member model
+class WriteMemberSerializer(serializers.ModelSerializer):
+    baseUser = LargeUserSerializer()
+
+    class Meta:
+        model = Member
+        fields = ['baseUser', 'profilePhoto', 'birthDate']
+
+
 # read-only, nestable serializer
 class SmallGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['name']
+
+
+# used for post/put/patch on the Group model
+class WriteGroupSerializer(serializers.GroupSerializer):
+    class Meta:
+        model = Group
+        fields = ['name', 'description']
