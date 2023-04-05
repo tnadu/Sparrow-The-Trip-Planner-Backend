6fc76ee60c6266ac83c6d24267645118ff08e4fe
asparagus <gigabyte7617@proton.me>
2023-04-05 14:54:37 +0300

    added Member Model and configured media directory and URL

--- sparrow/core/models.py
+++ sparrow/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# member model, extending the User model via a one-to-one relationship;
+# a member instance is generated whenever a user signs up, with both 
+# 'profilePhoto' and 'birthDate' fields set to null; 
+# these values can be set when updating the profile of a user;
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    profilePhoto = models.ImageField(upload_to='profile-photos', default='default-profile-photo.jpeg', db_column='profile_photo')
+    birthDate = models.DateField(null=True, db_column='birth_date')
+
+    class Meta:
+        ordering = ['user']
+        db_table = 'member'
+        default_related_name = 'member'
+
