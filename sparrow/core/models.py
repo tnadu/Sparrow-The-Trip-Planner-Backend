68412c53ac1623dbba45286cc7a3a27ea8c6f9ce
Radu <raduiris2007@gmail.com>
2023-04-05 18:22:15 +0300

    conflicts solved 2.0

--- sparrow/core/models.py
+++ sparrow/core/models.py
@@ -7,22 +7,17 @@ from datetime import datetime
 #from django.contrib.auth.models import User, Group
 
 class Route(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    verified = models.BooleanField(default=False)
-    public = models.BooleanField(default=False)
-    startingPointLat = models.FloatField()
-    startingPointLon = models.FloatField()
-    pub_date = models.DateTimeField('date published', auto_now_add=True)
-    user = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True, blank=True) # nullable, when the user is deleted the route isnt
-    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=True) #nullable
+    title = models.CharField(max_length=50, db_column='route_title')
+    description = models.CharField(max_length=3000, db_column='route_description')
+    verified = models.BooleanField(default=False, db_column='route_verified')
+    public = models.BooleanField(default=False, db_column='route_public')
+    startingPointLat = models.FloatField(db_column='route_starting_point_lat')
+    startingPointLon = models.FloatField(db_column='route_starting_point_lon')
+    publicationDate = models.DateTimeField('date published', auto_now_add=True, db_column='routePublicationDate')
+    user = models.ForeignKey(Member, on_delete=models.CASCADE, null=True, blank=True, db_column='route_user')  # nullable
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, db_column='route_group')  # nullable
     class Meta:
-        ordering = ['pub_date', 'user']
-
-    def change_starting_point(self, new_lat, new_lon):
-        self.startingPointLat = new_lat
-        self.startingPointLon = new_lon
-        self.save()
+        ordering = ['publicationDate', 'user']
 
     def __str__(self):
         return self.title + self.description
