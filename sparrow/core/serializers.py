19dc7b40c9885342080168d4b1f11dd831150c1f
Melania Ion <melaniaion24@gmail.com>
2023-04-20 00:11:53 +0300

    validation on two fields (route, attraction)

--- sparrow/core/serializers.py
+++ sparrow/core/serializers.py
@@ -23,6 +23,25 @@ class WriteRouteSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Only one of user and group can be specified")
         return data
 
+
+#used for write operations (post/put)
+class WriteRatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ['user', 'rating', 'comment', 'route', 'attraction']
+              
+    def validate(self, data):
+        route = data.get('route')
+        attraction = data.get('attraction')
+        
+        #only one and exactly one of the two nullable fields (route, attraction) can be null at a time
+        if route is not None and attraction is not None:
+            raise serializers.ValidationError("Only one of route or attraction can be specified.")
+        elif route is None and attraction is None:
+            raise serializers.ValidationError("Either route or attraction must be specified.")
+        
+        return data
+
 # retreives ALL the information for a a route
 class LargeRouteSerializer(serializers.ModelSerializer):
     author = SmallUserSerializer()
