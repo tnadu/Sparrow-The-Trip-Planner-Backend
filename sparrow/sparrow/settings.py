5a5fe2971ff5a8f117fe6635cea3a8d6e466f820
IoanaTC <ioanateodoraconstantin@yahoo.com>
2023-04-20 12:21:01 +0300

    media_root, media_url settings configured

--- sparrow/sparrow/settings.py
+++ sparrow/sparrow/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -138,6 +139,14 @@ REST_FRAMEWORK = {
     'DATE_INPUT_FORMATS': ['%d.%m.%Y'],
     'DATETIME_INPUT_FORMATS': ['%d.%m.%Y'],
 }
-    
-MEDIA_ROOT = '../media/'
-MEDIA_URL = 'media/'
+
+# the URL path where media files can be accessed
+MEDIA_URL = '/media/'
+
+# the absolute filesystem path to the directory that holds
+# user-uploaded media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
