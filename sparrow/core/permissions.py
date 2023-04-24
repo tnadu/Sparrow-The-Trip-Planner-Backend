4e61ac9ecd983554dfa4ed8c268ca7bc36efc865
Toma <92331145+playback0022@users.noreply.github.com>
2023-04-24 20:11:02 +0300

    small change

--- sparrow/core/permissions.py
+++ sparrow/core/permissions.py
@@ -1,25 +1,19 @@
 from rest_framework import permissions
 from .models import *
 
+
 class IsTheUserMakingTheRequest(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.baseUser
 
+
 class IsInGroup(permissions.BasePermission):    
     def has_object_permission(self, request, view, obj):
-        # when the user is trying to retrieve information about a group
-        # we should check if it belongs to the group
-        if request.method == 'GET':
-            mebmer_id = request.user.id
-            group_id = obj.id
+        mebmer_id = request.user.id
+        group_id = obj.id
 
-            try:
-                is_in_group = BelongsTo.objects.get(user_id = mebmer_id, group_id = group_id)
-                return True
-            except BelongsTo.DoesNotExist:
-                return False
-
-        # if he tries other actions, they will be denied, only
-        return False
-from .models import *
-
+        try:
+            is_in_group = BelongsTo.objects.get(user_id = mebmer_id, group_id = group_id)
+            return True
+        except BelongsTo.DoesNotExist:
+            return False
