c7301b4d5321923a4a1d8d4bbaaa2ce47e24f19a
Toma <92331145+playback0022@users.noreply.github.com>
2023-04-24 22:02:44 +0300

    some work on the update action left

--- sparrow/core/permissions.py
+++ sparrow/core/permissions.py
@@ -14,11 +14,11 @@ class IsOwnedByTheUserMakingTheRequest(permissions.BasePermission):
 
 class IsInGroup(permissions.BasePermission):    
     def has_object_permission(self, request, view, obj):
-        mebmer_id = request.user.id
+        member_id = request.user.id
         group_id = obj.id
 
         try:
-            is_in_group = BelongsTo.objects.get(user_id = mebmer_id, group_id = group_id)
+            is_in_group = BelongsTo.objects.get(user_id = member_id, group_id = group_id)
             return True
         except BelongsTo.DoesNotExist:
             return False
@@ -36,3 +36,35 @@ class IsAdminOfGroup(permissions.BasePermission):
         
         except BelongsTo.DoesNotExist:
             return False
+        
+
+class BelongsToAuthorization(permissions.BasePermission):
+    def has_permission(self, request, view):
+        validatedBelongsToData = view.get_serializer().context.get('data')
+        group = Group.objects.get(pk=validatedBelongsToData['group_id'])
+
+        return IsAdminOfGroup().check_object_permissions(request, group)
+    
+    def has_object_permission(self, request, view, obj):
+        group = Group.objects.get(pk=obj.group_id)
+        
+        if view.action == 'retrieve':
+            return IsInGroup().check_object_permissions(request, group)
+        
+        if view.action == 'destroy':
+            return IsAdminOfGroup().check_object_permissions(request, group)
+        
+        # 'update' and 'partial_update' action section
+        nickname = request.data.get('nickname')
+        modifiedUser = User.objects.get(pk=obj.user_id)
+
+        if nickname:
+            return IsTheUserMakingTheRequest().check_object_permissions(request, modifiedUser)
+
+        isAdmin = request.data.get('isAdmin')
+
+        if isAdmin:
+            return IsAdminOfGroup().check_object_permissions(request, group)
+        
+        return True
+
