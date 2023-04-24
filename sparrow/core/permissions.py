45961e13937a6f21d827a6d68edab881967a9805
VladWero08 <vlad_wero@yahoo.com>
2023-04-24 17:38:40 +0300

    fixed typos

--- sparrow/core/permissions.py
+++ sparrow/core/permissions.py
@@ -5,12 +5,12 @@ class IsAdminOfGroup(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # when the user is trying to perform action on a group
         # we should check if it belongs to the group
-        if request.method == 'POST' or request.method == 'PUT' or request.method == 'PATCH' or request.methods == 'DELETE':
+        if request.method == 'POST' or request.method == 'PUT' or request.method == 'PATCH' or request.method == 'DELETE':
             mebmer_id = request.user.id
             group_id = obj.id
 
             try:
-                is_in_group = BelongsTo.objects.get(user_id = mebmer_id, group_id = group_id)
+                is_in_group = BelongsTo.objects.get(member_id = mebmer_id, group_id = group_id)
                 # if the user is admin, it should have isAdmin set to true
                 return is_in_group.isAdmin
             
