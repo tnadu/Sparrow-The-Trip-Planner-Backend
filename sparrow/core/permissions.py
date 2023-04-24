93cddfbf2af21283830dba8ef57dbff12328754e
Toma <92331145+playback0022@users.noreply.github.com>
2023-04-24 21:35:44 +0300

    written, awaiting testing

--- sparrow/core/permissions.py
+++ sparrow/core/permissions.py
@@ -3,9 +3,15 @@ from .models import *
 
 
 class IsWithinAuthorization(permissions.BasePermission):
+    # will validate 'create' requests
     def has_permission(self, request, view):
-        
+        # access the serialized and validated IsWithin data
+        validatedIsWithinData = view.get_serializer().context['data']
+        route = Route.objects.get(pk=validatedIsWithinData['route_id'])
 
+        return RouteIsAuthorizedToMakeChanges().has_object_permission(request, view, route)
+
+    # validates all other allowed actions
     def has_object_permission(self, request, view, obj):
         route = Route.objects.get(pk=obj.route_id)
 
