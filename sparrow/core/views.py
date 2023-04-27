d78c4fa756a54c3c84d23e720559e7572c99a0a3
Radu <raduiris2007@gmail.com>
2023-04-27 16:23:01 +0300

    updated the get_permissions to its final form with all cases

--- sparrow/core/views.py
+++ sparrow/core/views.py
@@ -23,6 +23,11 @@ class RouteViewSet(ModelViewSet):
         return RouteSerializer
     
     def get_permissions(self):
+        # route can be accessed only if it is public
+        if self.action == 'list' or self.action == 'retrieve':
+            return[RouteIsPublic]
+        
+        # edited or deleted only if admin or admin of the group
         if self.action == 'update' or self.action == 'patch' or self.action == 'delete':
             return [RouteIsAuthorizedToMakeChanges]
 
