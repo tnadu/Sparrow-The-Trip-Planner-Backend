db632636527ffcd57e8f88bffa1f386674db1d32
Melania Ion <melaniaion24@gmail.com>
2023-04-28 19:05:16 +0300

    authorization - ratingFlag

--- sparrow/core/permissions.py
+++ sparrow/core/permissions.py
@@ -36,3 +36,20 @@ class IsAdminOfGroup(permissions.BasePermission):
         
         except BelongsTo.DoesNotExist:
             return False
+
+class RatingFlagAuthorization(permissions.BasePermission):    
+    def has_object_permission(self, request, view, obj):
+        rating_flag = RatingFlag.objects.get(pk = obj.id)
+        
+        # if it's a rating on an attraction, check if the user is authenticated
+        # else if it's a rating on a route, check the permission for that route (public/private)
+        if view.action in ['list','create','retrieve']:
+            if rating_flag.attraction:
+                return request.user.is_authenticated
+            else:
+                return (RouteIsPublic().has_object_permission(request, view, rating_flag.route))
+        
+        # only the user who rated the object is allowed to update/delete  
+        if view.action in ['update','partial_update','destroy']:
+            if rating_flag.attraction or rating_flag.route:
+                return (IsOwnedByTheUserMakingTheRequest().has_object_permission(request, view, rating_flag))
