0c573838e072cc0bca8f76b15760c6132daa8d97
IoanaTC <ioanateodoraconstantin@yahoo.com>
2023-06-15 14:02:55 +0300

    admin init

--- sparrow/core/admin.py
+++ sparrow/core/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Route
+from .models import *
+
 
 # Register your models here.
 admin.site.register(Route)
+admin.site.register(isWithin)
+admin.site.register(Attraction)
+                    
+admin.site.register(Member)
+admin.site.register(Group)
+admin.site.register(BelongsTo)
+admin.site.register(Status)
+admin.site.register(Notebook)
+admin.site.register(Tag)
+admin.site.register(RatingFlagType)
+admin.site.register(IsTagged)
+admin.site.register(RatingFlag)
+admin.site.register(Image)
