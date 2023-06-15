c066a09ff6857978679398c300b804f9de62bd3a
Toma <92331145+playback0022@users.noreply.github.com>
2023-06-15 11:57:41 +0300

    created mock DB dataset using fixtures

--- sparrow/core/tests.py
+++ sparrow/core/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import *
 
-# Create your tests here.
+
+class IsTaggedTests(APITestCase):
+    fixtures = ['testing-members.json', 'testing-groups.json', 'testing-belongsTo.json', 'testing-attractions.json', 'testing-routes.json']
+
+    def testConcurrentTagging(self):
+        """
+        Verify that any two users can concurrently change the tags of an attraction
+        """
+
+        # tagURL = reverse("isTagged-list")
